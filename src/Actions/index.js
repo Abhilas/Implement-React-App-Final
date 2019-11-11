f3549082d7bf3dce667797fa80f2490d1be13155
Abhilas <abhi.mohanty86@gmail.com>
2019-11-11 14:27:53 +0530

    Update index.js

--- src/Actions/index.js
+++ src/Actions/index.js
@@ -1,6 +1,11 @@
 import ServerAPI from "../API/JSONPlaceHolder";
 import History from "../AppRoute/history";
-import { REGISTER_USER, CHECK_USERNAME } from "./actionTypes";
+import {
+  REGISTER_USER,
+  CHECK_USERNAME,
+  LOGIN_USER,
+  LOGIN_ERROR
+} from "./actionTypes";
 
 export const registerUser = userDetails => {
   return async dispatch => {
@@ -16,12 +21,34 @@ export const registerUser = userDetails => {
 };
 
 export const loginUser = userDetails => {
+  console.log("userDetails from Login ==> ", userDetails);
   return async dispatch => {
-    const response = await ServerAPI.get("/users");
+    const response = await getAllUsers();
 
-    dispatch({
-      type: REGISTER_USER,
-      payload: response
+    console.log("all Users ==> ", response);
+
+    const reqUser = response.data.filter(eachUser => {
+      return (
+        eachUser.username === userDetails.username &&
+        eachUser.password === userDetails.password
+      );
     });
+
+    if (reqUser.length > 0) {
+      dispatch({
+        type: LOGIN_USER,
+        payload: reqUser[0]
+      });
+      History.push("/");
+    } else {
+      dispatch({
+        type: LOGIN_ERROR,
+        payload: "Wrong username & password !!!"
+      });
+    }
   };
+};
+
+export const getAllUsers = () => {
+  return ServerAPI.get("/users");
 };
