bc92755b6347c4613ccc52189ddf2f7f82bcc6c1
Abhilas <abhi.mohanty86@gmail.com>
2019-11-11 14:27:28 +0530

    Update actionTypes.js

--- src/Actions/actionTypes.js
+++ src/Actions/actionTypes.js
@@ -3,4 +3,5 @@ export const REGISTER_USER = "REGISTER_USER";
 export const CHECK_USERNAME = "CHECK_USERNAME";
 
 // For Login
-export const LOGIN = "LOGIN";
+export const LOGIN_USER = "LOGIN_USER";
+export const LOGIN_ERROR = "LOGIN_ERROR";
